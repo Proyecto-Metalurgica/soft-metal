3b2a34d2267e4a3855f482b73a4967c9bba0bc36
Caro <carobaeza85@gmail.com>
2020-08-19 00:40:30 -0300

    Se crea clase Material con sus atributos y layout

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/Material.java
@@ -0,0 +1,124 @@
+package domainapp.modules.simple.dom.impl;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import com.google.common.collect.ComparisonChain;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.Auditing;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.annotation.Title;
+import org.apache.isis.applib.services.i18n.TranslatableString;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+
+import lombok.AccessLevel;
+import static org.apache.isis.applib.annotation.CommandReification.ENABLED;
+import static org.apache.isis.applib.annotation.SemanticsOf.IDEMPOTENT;
+import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE;
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE, schema = "simple")
+@javax.jdo.annotations.DatastoreIdentity(strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column="id")
+@javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column="version")
+
+@javax.jdo.annotations.Unique(name="Material_name_UNQ", members = {"tipo"})
+@DomainObject(auditing = Auditing.ENABLED)
+@DomainObjectLayout()  // causes UI events to be triggered
+@lombok.Getter @lombok.Setter
+@lombok.RequiredArgsConstructor
+
+public class Material implements Comparable<Material> {
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property() // editing disabled by default, see isis.properties
+    @Title(prepend = "Material: ")
+    private String tipo;
+
+    @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String medida;
+
+    @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String unidad;
+
+    @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String precio;
+
+
+
+    @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith = "tipo")
+    public Material updateTipo(
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Tipo") final String tipo,
+            @ParameterLayout(named = "Medida") final String medida,
+            @ParameterLayout(named = "Unidad") final String unidad,
+            @ParameterLayout(named = "Precio") final String precio){
+        setTipo(tipo);
+        setMedida(medida);
+        setUnidad(unidad);
+        setPrecio(precio);
+
+        return this;
+    }
+
+    public String default0UpdateTipo() {
+        return getTipo();
+    }
+
+    public TranslatableString validate0UpdateTipo(final String tipo) {
+        return tipo != null && tipo.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
+    }
+
+
+    @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' deleted", title));
+        repositoryService.remove(this);
+    }
+
+
+    @Override
+    public String toString() {
+        return getTipo();
+    }
+
+
+
+    public int compareTo(final Material other) {
+        return ComparisonChain.start()
+                .compare(this.getTipo(), other.getTipo())
+                .result();
+    }
+
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    TitleService titleService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    MessageService messageService;
+
+}
---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/Materiales.java
@@ -0,0 +1,97 @@
+
+package domainapp.modules.simple.dom.impl;
+
+        import java.util.List;
+
+        import org.datanucleus.query.typesafe.TypesafeQuery;
+
+        import org.apache.isis.applib.annotation.Action;
+        import org.apache.isis.applib.annotation.ActionLayout;
+        import org.apache.isis.applib.annotation.BookmarkPolicy;
+        import org.apache.isis.applib.annotation.DomainService;
+        import org.apache.isis.applib.annotation.DomainServiceLayout;
+        import org.apache.isis.applib.annotation.MemberOrder;
+        import org.apache.isis.applib.annotation.NatureOfService;
+        import org.apache.isis.applib.annotation.ParameterLayout;
+        import org.apache.isis.applib.annotation.Programmatic;
+        import org.apache.isis.applib.annotation.SemanticsOf;
+        import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
+        import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+        import org.apache.isis.applib.services.repository.RepositoryService;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "simple.SimpleObjectMenus",
+        repositoryFor = Material.class
+)
+@DomainServiceLayout(
+        named = "Materiales",
+        menuOrder = "10"
+)
+public class Materiales {
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "1")
+    public List<Material> listAll() {
+        return repositoryService.allInstances(Material.class);
+    }
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "2")
+    public List<Material> findByName(
+            @ParameterLayout(named = "Tipo") final String tipo
+    ) {
+        TypesafeQuery<Material> q = isisJdoSupport.newTypesafeQuery(Material.class);
+        final QMaterial cand = QMaterial.candidate();
+        q = q.filter(
+                cand.tipo.indexOf(q.stringParameter("tipo")).ne(-1)
+        );
+        return q.setParameter("tipo", tipo)
+                .executeList();
+    }
+
+    @Programmatic
+    public Material findByNameExact(final String tipo) {
+        TypesafeQuery<Material> q = isisJdoSupport.newTypesafeQuery(Material.class);
+        final QMaterial cand = QMaterial.candidate();
+        q = q.filter(
+                cand.tipo.eq(q.stringParameter("tipo"))
+        );
+        return q.setParameter("tipo", tipo)
+                .executeUnique();
+    }
+
+    @Programmatic
+    public void ping() {
+        TypesafeQuery<Material> q = isisJdoSupport.newTypesafeQuery(Material.class);
+        final QMaterial candidate = QMaterial.candidate();
+        q.range(0, 2);
+        q.orderBy(candidate.tipo.asc());
+        q.executeList();
+    }
+
+    public static class CreateDomainEvent extends ActionDomainEvent<Materiales> {
+    }
+
+    @Action(domainEvent = CreateDomainEvent.class)
+    @MemberOrder(sequence = "3")
+    public Material create(
+            @ParameterLayout(named = "Tipo") final String tipo,
+            @ParameterLayout(named = "Medida") final String medida,
+            @ParameterLayout(named = "Unidad") final String unidad,
+            @ParameterLayout(named = "Precio") final String precio
+
+    ) {
+        return repositoryService.persist(new Material(tipo, medida, unidad, precio));
+    }
+
+    @javax.inject.Inject
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
+
+}
