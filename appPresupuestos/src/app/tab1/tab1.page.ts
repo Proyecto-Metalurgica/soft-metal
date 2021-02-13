b5ae39c29285133878dcdc16ea5dea6ef80a8e26
Caro <carobaeza85@gmail.com>
2021-02-13 12:49:28 -0300

    Issue #42 :Conexion de appmovil a base de datos

--- appPresupuestos/src/app/tab1/tab1.page.ts
+++ appPresupuestos/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+
+      public resultadosArray : any = null;;
+
+
+       ngOnInit() {
+        this.listarTodasLasOT();
+      }
+
+     listarTodasLasOT(){
+           const httpOptions = {
+             headers: new HttpHeaders({
+               'Accept':  'application/json;profile=urn:org.apache.isis/v1',
+               'Authorization': 'Basic c3ZlbjpwYXNz',
+             })
+           }
+           const URL = 'http://localhost:8080/restful/services/simple.OrdenTrabajoMenu/actions/listAll/invoke';
+           this.http.get(URL, httpOptions)
+           .subscribe(resultados => (this.resultadosArray = resultados)
+           );
+
+         }
+
+      filterItemsOfType(){
+        return this.resultadosArray.filter(resultado => resultado.titulo != null);
+      }
 
 }
