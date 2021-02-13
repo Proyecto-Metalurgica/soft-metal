83eddc45de7f2ace0b61103687a7e4091f8c9403
Caro <carobaeza85@gmail.com>
2021-02-12 23:59:57 -0300

    Issue #34 : Se cambian colores y se elimina tab3

--- appPresupuestos/src/app/tabs/tabs-routing.module.ts
+++ appPresupuestos/src/app/tabs/tabs-routing.module.ts
@@ -15,10 +15,7 @@ const routes: Routes = [
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
+
       {
         path: '',
         redirectTo: '/tabs/tab1',
